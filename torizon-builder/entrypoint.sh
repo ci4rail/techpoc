b1e212854bf59cfa422bb2e9a7dc2f9a1d7d1a38
Armin Schlegel <armin.schlegel@gmx.de>
2020-09-11 08:53:42 +0200

    added build.sh, simplified scripts

--- torizon-builder/entrypoint.sh
+++ torizon-builder/entrypoint.sh
@@ -2,41 +2,6 @@
 set -o errexit   # abort on nonzero exitstatus
 set -o pipefail  # don't hide errors within pipes
 
-if [ -z "${USERID}" ]; then
-  echo "No user id supplied. Using default id '9001'"
-  USERID=9001
-fi
-
-if [ -z "${WORK_DIR}" ]; then
-  WORK_DIR=/workdir
-  echo "No WORK_DIR defined. Defaulting to '${WORK_DIR}'"
-fi
-
-if [ -z "${BUILD_DIR}" ]; then
-  BUILD_DIR=build-torizon
-  echo "No BUILD_DIR defined. Defaulting to '${BUILD_DIR}'"
-fi
-
-if [ -z "${DISTRIBUTION}" ]; then
-  DISTRIBUTION=torizon
-  echo "No DISTRIBUTION defined. Defaulting to '${DISTRIBUTION}'"
-fi
-
-if [ -z "${BRANCH}" ]; then
-  BRANCH=master
-  echo "No BRANCH defined. Defaulting to '${BRANCH}'"
-fi
-
-if [ -z "${MACHINE}" ]; then
-  MACHINE=verdin-imx8mm
-  echo "No MACHINE defined. Defaulting to '${MACHINE}'"
-fi
-
-if [ -z "${MANIFEST_REPO}" ]; then
-  MANIFEST_REPO=https://github.com/ci4rail/toradex-torizon-manifest
-  echo "No MANIFEST_REPO defined. Defaulting to '${MANIFEST_REPO}'"
-fi
-
 useradd --shell /bin/bash -u ${USERID} -o -c "" -m user
 
 # Configure Git if not configured
--- torizon-builder/prepare.sh
+++ torizon-builder/prepare.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
+set -o errexit   # abort on nonzero exitstatus
+set -o pipefail  # don't hide errors within pipes
+
+if [ -z "${USERID}" ]; then
+  echo "No user id supplied. Using default id '9001'"
+  USERID=9001
+fi
+
+if [ -z "${WORK_DIR}" ]; then
+  WORK_DIR=/workdir
+  echo "No WORK_DIR defined. Defaulting to '${WORK_DIR}'"
+fi
+
+if [ -z "${BUILD_DIR}" ]; then
+  echo "No BUILD_DIR defined. Exiting."
+  exit 1
+fi
+
+if [ -z "${DISTRIBUTION}" ]; then
+  echo "No DISTRIBUTION defined. Exiting."
+  exit 1
+fi
+
+if [ -z "${BRANCH}" ]; then
+  echo "No BRANCH defined. Exiting."
+  exit 1
+fi
+
+if [ -z "${MACHINE}" ]; then
+  echo "No MACHINE defined. Exiting."
+  exit 1
+fi
+
+if [ -z "${MANIFEST_REPO}" ]; then
+  echo "No MANIFEST_REPO defined. Exiting."
+  exit 1
+fi
 
 mkdir -p ${WORK_DIR}/${DISTRIBUTION}
 cd ${WORK_DIR}/${DISTRIBUTION}
@@ -7,12 +44,12 @@ cd ${WORK_DIR}/${DISTRIBUTION}
 repo status 2> /dev/null
 if [ "$?" = "1" ]
 then
-    repo init -u ${MANIFEST_REPO} -b $BRANCH
+    repo init -u ${MANIFEST_REPO} -b ${BRANCH}
     repo sync
 fi # Do not sync automatically if repo is setup already
 
 # Initialize build environment
-MACHINE=$MACHINE source setup-environment
+source setup-environment
 
 # Accept Freescale/NXP EULA
 if ! grep -q ACCEPT_FSL_EULA ${WORK_DIR}/${DISTRIBUTION}/${BUILD_DIR}/conf/local.conf 
